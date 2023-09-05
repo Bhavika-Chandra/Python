c32276d9a1eed8611fb1d62709148d92e2d179d2
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2023-09-05 17:42:19 +0530

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,5 +1,5 @@
 import pytube
 link = input('Enter youtube video URL')
 yt = pytube.YouTube(link)
-yt.streams.first().download()
-print('downloaded', link)
+yt.streams.first().download() #File will get downloaded in that folder where you have saved this program
+print('downloaded', link) 
