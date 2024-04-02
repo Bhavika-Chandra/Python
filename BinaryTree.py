5cd1487bfeef3ef8f1d2a24e1dc1bff70cebd6ba
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2024-04-02 16:00:13 +0530

    Added both DFS and BFS approaches.

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,5 +1,5 @@
-#Q.Maximum depth of binary tree
-#Iterative DFS solution (uses stack)
+#Q.104) Leetcode - Maximum depth of binary tree
+#Iterative BFS solution (uses queue), Also called as Level order Traversal
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
@@ -15,3 +15,17 @@ class Solution:
                     q.append(node.right)
             level += 1
         return level
+
+#Iterative DFS solution (uses stack)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        result = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                result = max(result, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return result
