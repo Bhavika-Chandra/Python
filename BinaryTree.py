b0b38993ac7f09a0d3137c26b7c260e976462fb6
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2024-04-02 15:46:29 +0530

    Create BinaryTree.py

---
+++ BinaryTree.py
@@ -0,0 +1,17 @@
+#Q.Maximum depth of binary tree
+#Iterative DFS solution (uses stack)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
